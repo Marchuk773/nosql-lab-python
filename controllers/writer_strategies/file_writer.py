95b731cb35955e888a67f8f3d398a390d4f91711
Marchuk Roman <marchukroman228@gmail.com>
2022-06-13 16:31:27 +0300

    Fixed logic with writing to a file

--- controllers/writer_strategies/file_writer.py
+++ controllers/writer_strategies/file_writer.py
@@ -10,15 +10,13 @@ class FileWriter(BaseWritter):
     def __init__(self, identifier):
         super().__init__(identifier, destination='file')
         self.filepath = OUTPUT_DESTINATION
-        self.append = False
 
     def process(self, content, offset):
         if self.is_chunk_processed(offset):
-            self.append = True
             return
 
         os.makedirs(os.path.basename(self.filepath), exist_ok=True)
-        with open(os.path.join(self.filepath, f"{self.identifier}_{offset}.json"), "a" if self.append else "w") as output_f:
+        with open(os.path.join(self.filepath, f"{self.identifier}_{offset}.json"), "w") as output_f:
             dump(content, output_f)
             output_f.write("\n")
 
